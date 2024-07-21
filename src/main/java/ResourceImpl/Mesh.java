96a458ea1a837d77df7f7e3d6c2f40c36f1ce97d
emeraldDevUA <kokhanchuk2021ks13@student.karazin.ua>
2024-07-21 22:13:40 +0200

    VBOs

--- src/main/java/ResourceImpl/Mesh.java
+++ src/main/java/ResourceImpl/Mesh.java
@@ -1,10 +1,52 @@
 package ResourceImpl;
 
+import Interfaces.Drawable;
 import Interfaces.EnhancedLoadable;
+import org.joml.Vector2d;
+import org.joml.Vector3d;
+import org.joml.Vector4d;
+
 import java.io.FileNotFoundException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
+class VBO implements Drawable {
+    private ByteBuffer modelBuffer;
+    public VBO(){
+
+    }
+
+    public void putVertex(Vector4d vector){
+
+    }
+    public void putNormal(Vector3d vector){
+
+    }
+    public void putTex(Vector2d vector){
+
+    }
+
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void compile() {
+
+    }
+
+}
 
 public class Mesh implements EnhancedLoadable {
-    
+    private final Map<String, VBO> map;
+
+    public Mesh(){
+        map = new HashMap<>();
+    }
+
     @Override
     public void load(String path) throws FileNotFoundException {
 
@@ -12,7 +54,9 @@ public class Mesh implements EnhancedLoadable {
 
     @Override
     public void assemble() {
-
+        for (VBO vbo : map.values()) {
+            vbo.compile();
+        }
     }
 
 }
